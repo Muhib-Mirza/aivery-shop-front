f1b43e7c225dad08f2e81d3bec5b79ad7217f682
WaqasMir <109218668+Muhib-Mirza@users.noreply.github.com>
2023-01-21 13:33:16 +0500

    fixes

--- pages/admin/bird/8253148976253.js
+++ pages/admin/bird/8253148976253.js
@@ -43,6 +43,23 @@ const AddBird = () => {
             <input className={`${style.file} ${style.margin}`} type="file" onChange={handleImage} />
             <button className={`${style.margin} ${style.btn}`} type="submit">Submit</button>
         </form>
+        <div className={style.list}>
+            <h3 className={style.head} >Bird ID</h3>
+        <ul className={style.ul} >
+            <li className={style.listitem}>Albino Black Eye = ab</li>
+            <li className={style.listitem}>Albino Red Eye = ar</li>
+            <li className={style.listitem}>Pastle Split ino = psi</li>
+            <li className={style.listitem}>Cremino = crm</li>
+            <li className={style.listitem}>Pastle Ino = pin</li>
+            <li className={style.listitem}>Green Opaline = gop</li>
+            <li className={style.listitem}>Green Euwing Opalines = geo</li>
+            <li className={`${style.listitem} ${style.incwidth}`}>Green Opalines Split Blue = gosb</li>
+            <li className={style.listitem}>Green Euwing / blue = gesb</li>
+            <li className={style.listitem}>Blue Euwing Opalines = beo</li>
+            <li className={style.listitem}>Parblue Opalines = po</li>
+            <li className={style.listitem}>Green/ opaline/Parblue = gsosp</li>
+        </ul>
+        </div>
         </div>
         </>
      );

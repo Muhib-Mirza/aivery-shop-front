dec796fba8810355f4f08299c85d8d3fb5421c41
WaqasMir <109218668+Muhib-Mirza@users.noreply.github.com>
2023-01-22 15:27:54 +0500

    bug fixes

--- pages/[bird].js
+++ pages/[bird].js
@@ -2,44 +2,18 @@ import Head from "next/head";
 import style from "../styles/Bird.module.css";
 import {motion} from "framer-motion";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
-export const getStaticPaths = async ()=>{
-    const res = await fetch("https://morning-star-aivary.onrender.com/alldata");
-    const data = await res.json();
-    const paths = data.map(curElem=>{
-        return{
-            params:{
-                bird:curElem.birdid
-            }
-        }
-    });
-
+export async function getServerSideProps(context){
+    const {bird} = context.query;
+    const res = await axios.get("https://morning-star-aivary.onrender.com/"+bird);
     return{
-        paths,
-        fallback: false,
+    props:{
+        data:res.data
     }
 }
-
-export const getStaticProps = async ( context )=>{
-    let data= context.params.bird;
-    return{
-        props:{
-            data:data
-        }
-    }
 }
 
-const Bird = ({ data }) => {
-    getStaticPaths;
-    const [state,setState] = useState(null);
-    const [flag, setFlag] = useState(0);
-    useEffect(()=>{
-        const res = axios.get("https://morning-star-aivary.onrender.com/"+data).then((result)=>{
-        setState(result.data);
-        setFlag(1);
-    })
-    },[]);
+const Test = ({data}) => {
     return ( 
         <>
         <Head>
@@ -49,40 +23,38 @@ const Bird = ({ data }) => {
         <link rel="icon" href="/parrot.png" />
       </Head>
       <div className={`${style.container}`}>
-        { state === null?<div class="loader">
-        <div class="circle"></div>
-        </div> : state.map(curElem=>{
-            const image = btoa(
-                String.fromCharCode(...new Uint8Array(curElem.image.data.data)));
-                return <div key={curElem._id}>
-                    <div className={style.ccontainer}>
-                    <div className={style.card}>
-                    <img src={`data:img/png;base64,${image}`} alt="" className={`${style.image}`} />
-                    <div className={`${style.cbody}`}>
-                        <h4 className={style.birdName}>{curElem.birdname} </h4>
-                    <div>
-                        <span className={style.cos}>Cost :</span>
-                        <span className={style.cost}>
-                        {curElem.birdcost} 
-                        </span>
-                        <span className={style.unit}>
-                            Rs
-                        </span>
-                        </div>
-                        <a href="tel:+923070408790">
-                        <motion.button className={style.btn} whileHover={{scale:1.1, backgroundColor:"grey", color:"black",fontWeight:"bold"}} >
-                            Buy Now
-                        </motion.button>
-                        </a>
+    { data.map(curElem=>{
+        const image = btoa(
+            String.fromCharCode(...new Uint8Array(curElem.image.data.data)));
+            return <div key={curElem._id}>
+                <div className={style.ccontainer}>
+                <div className={style.card}>
+                <img src={`data:img/png;base64,${image}`} alt="" className={`${style.image}`} />
+                <div className={`${style.cbody}`}>
+                    <h4 className={style.birdName}>{curElem.birdname} </h4>
+                <div>
+                    <span className={style.cos}>Cost :</span>
+                    <span className={style.cost}>
+                    {curElem.birdcost} 
+                    </span>
+                    <span className={style.unit}>
+                        Rs
+                    </span>
                     </div>
+                    <a href="tel:+923070408790">
+                    <motion.button className={style.btn} whileHover={{scale:1.1, backgroundColor:"grey", color:"black",fontWeight:"bold"}} >
+                        Buy Now
+                    </motion.button>
+                    </a>
                 </div>
-                </div>
-                </div>
-        })
-    }
-        </div>
+            </div>
+            </div>
+            </div>
+    })
+}
+    </div>       
         </>
      );
 }
  
-export default Bird;
+export default Test;

cb897b5418e1c3da7fc51a29630b67f038318cbe
WaqasMir <109218668+Muhib-Mirza@users.noreply.github.com>
2023-01-27 20:16:09 +0500

    bug fixes

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,18 @@ import Carousel from './Components/Slider'
 export default function Home() {
   const [flag,setFlag] = useState(0);
   useEffect(()=>{
-    setFlag(1);
-  },[]);
+    const timer = setTimeout(() => {
+      setFlag(1);
+      }, 500);
+      return () => clearTimeout(timer);
+  });
   return (
     <>
       <Head>
         <title>Morning Star Aivary</title>
         <meta name="description" content="We have best quality of love birds" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="bird, Love bird, aivary, Morning star aivary" />
         <link rel="icon" href="/parrot.png" />
       </Head>
       {
